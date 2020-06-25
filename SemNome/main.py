9e3a3515c5d19a179428e75f63af6f38cbbbbabe
CarlosHenrique13 <carloshenriquexx123@gmail.com>
2020-06-25 17:46:08 -0300

    base do projeto

---
+++ SemNome/main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from config.conf import *
+from config.user import *
+
+#Criação da Janela
+janela = Tk()
+janela.title("Projeto Nome")
+janela.geometry("500x400")
+janela.mainloop()
