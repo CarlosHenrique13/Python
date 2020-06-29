3bc0dc2c7ea69e7c05b8119810fc692819af0a40
CarlosHenrique13 <carloshenriquexx123@gmail.com>
2020-06-29 17:21:34 -0300

    slid 29/06/20 17:21

--- SemNome/complet.py
+++ SemNome/complet.py
@@ -6,14 +6,11 @@ import os
 def BarraMenu(janela):
     barraMenu = Menu(janela)
     #Cria a categooria
-    items1 = Menu(barraMenu)
     slid = Menu(barraMenu)
     #Itens
-    items1.add_command(label='Calculadora')
     slid.add_command(label="Escrever",command=lambda: EscreverSlid())
     slid.add_command(label="Ler",command=lambda: LeitorSlid())
     #Colocar categoria no menu
-    barraMenu.add_cascade(label='Conexão', menu=items1)
     barraMenu.add_cascade(label='Slid',menu=slid)
     #configuração da janela *unico
     janela.config(menu=barraMenu)
--- SemNome/config/slid/__init__.py
+++ SemNome/config/slid/__init__.py
@@ -3,23 +3,23 @@ from tkinter import ttk
 import os
 
 
+if not os.path.exists("Armazena/Slids"):
+    os.makedirs("Armazena/Slids")
+
+
 #Escrever Arquivo
 def EscreverSlid():
     #Cria o Arquivo e Ecrever
-    def Escrever(arquivo,texto):
+    def Escreve(arquivo,texto):
         arg = open(f"Armazena/Slids/{arquivo}.txt","wt+")
-        #for c in range(0,len(texto)):
         arg.write(texto)
-
-
-
 
     janela = Tk()
     janela.geometry("300x210")
     Label(janela,text="Nome do Arquivo: ").place(x=5,y=5)
     arg_name = Entry(janela)
     arg_name.place(x=120,y=5)
-    btn = Button(janela, text="Enviar", command=lambda: Escrever(arg_name.get(),escrever.get(1.0, END)))
+    btn = Button(janela, text="Enviar", command=lambda: Escreve(arg_name.get(),escrever.get(1.0, END)))
     btn.place(x=250,y=5)
     escrever = Text(janela,width=35,height=10)
     escrever.place(x=5,y=35)
@@ -27,13 +27,18 @@ def EscreverSlid():
 
 #Ler o Arquivo
 def LeitorSlid():
-    pass
+    def Ler(name):
+        arg = open(f"Armazena/Slids/{name}","rt")
+        linhas = arg.read()
+        texto.insert(1.0,linhas)
 
-#Função Principal
-def Slid():
     janela = Tk()
-    arg_name = Entry()
-    arg_name.grid(0,0)
-    escreve = Entry()
-    escreve.grid(0,1)
+    janela.geometry("340x210")
+    Label(janela, text="Nome do Arquivo: ").place(x=5, y=5)
+    arg_name = ttk.Combobox(janela,value=os.listdir("Armazena/Slids/"))
+    arg_name.place(x=120, y=5)
+    btn = Button(janela, text="Proximo", command=lambda: Ler(arg_name.get()))
+    btn.place(x=270, y=5)
+    texto = Text(janela, width=40, height=10)
+    texto.place(x=5, y=35)
     janela.mainloop()
