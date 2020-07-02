138f6913b31d5daf3be7f8140b866e0f406ff9b1
CarlosHenrique13 <carloshenriquexx123@gmail.com>
2020-07-02 15:47:15 -0300

    erro nçao encontra o arquivo 02/07/20 15:47

--- SemNome/config/instalador/__init__.py
+++ SemNome/config/instalador/__init__.py
@@ -22,5 +22,9 @@ def InstalardorInic(janela):
     apps  = os.listdir("Armazena/Programas")
     programas = []
     for c in range(0,len(apps)):
-        programas.append(f"Armazena/Programas/{apps[c]}/{apps[c]}")    
-
+        programas.append(f"Armazena\Programas\{apps[c]}")    
+    
+    for c in range(0,len(apps)):
+        print(f"{programas[c]}\{apps[c]}")
+        btn = Button(janela,text=apps[c],command=lambda: os.startfile(f"{os.getcwd()}\{programas[c]}\{apps[c]}"))
+        btn.grid(row=0,column=c)
