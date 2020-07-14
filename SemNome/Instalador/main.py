92d6d59d969dd9fbe229ddd5ffff1c0b4149db14
CarlosHenrique13 <62728022+CarlosHenrique13@users.noreply.github.com>
2020-07-14 18:47:35 -0300

    main

--- SemNome/Instalador/main.py
+++ SemNome/Instalador/main.py
@@ -4,11 +4,12 @@ from classes import *
 
 #Externas
 import os
-
+from tkinter import *
 
 #Variaveis
-DIRETORIA = "Armazena"
-NOMES = os.listdir("Armazena")
+DIRETORIA = "apps"
+NOMES = os.listdir("apps")
+
 
 
 #Busca das Aplicação dentro da diretoria
@@ -16,8 +17,65 @@ arquivos         = Buscar(NOMES)
 class_Instalador = Instal_SHINC(DIRETORIA,NOMES,arquivos)
 
 #Leitura do Arquivo de execução e separação de comandos
-conteudo = class_Instalador.LerArg()
-posisao  = class_Instalador.SeparaComentario(conteudo)
-comando  = class_Instalador.LimpComentario(conteudo,posisao)
+conteudo  = class_Instalador.LerArg()
+posissao  = class_Instalador.SeparaComentario(conteudo)
+comandos  = class_Instalador.LimpComentario(conteudo,posissao)
+
+janela = Tk()
+for c in range(0,len(NOMES)):
+    a = class_Instalador.ExecutaComando(janela,comandos)
+    a[NOMES[c]][0].pack()
+
+janela.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+janela = Tk()
+btn = [Button(janela,text=NOMES[0],command=lambda: class_Instalador.ExecutaComando(comandos[NOMES[0]])),
+       Button(janela,text=NOMES[1],command=lambda: class_Instalador.ExecutaComando(comandos[NOMES[1]]))]
+btna = {}
+for c in range(0,len(NOMES)):
+    btna[NOMES[c]]= Button(janela,text=NOMES[c],command=lambda: os.startfile(f'apps\{NOMES[c]}\{NOMES[c]}.py'))
+print(btn)
+print(btna)
+
+#for c in range(0,len(NOMES)):
+btn[0].pack()
+btn[1].pack()
+btna['meuapp'].pack()
+janela.mainloop()
+
+"""
+
+
 
 #Execução dos Arquivos
