45495627cd40e85bb2998e1e110742549db3887f
CarlosHenrique13 <62728022+CarlosHenrique13@users.noreply.github.com>
2020-08-04 17:04:55 -0300

    ssh interfa

--- ssh/src/interface/__init__.py
+++ ssh/src/interface/__init__.py
@@ -1,21 +1,31 @@
+#Pacotes Enternos
 from tkinter import *
+
+#Pacotes Interno
+from src.ssh import *
 
 #Janela
 janela = Tk()
 janela.title("SSH")
+janela.geometry("500x300+50+50")
 conetc = Label(janela,text="Conectado: 127.0.0.1")
 conetc.pack()
 #Host Entrada
-Label(janela,text="Ip ou Domínio").pack()
+Label(janela,text="Ip ou Domínio").place(x=5,y=20)
 host = Entry(janela)
-host.pack()
+host.place(x=5,y=40)
 #Usuario Entrada
+Label(janela,text="Usuario").place(x=150,y=20)
 user = Entry(janela)
-user.pack()
+user.place(x=150,y=40)
 #Senha Entrada
+Label(janela,text="Senha").place(x=290,y=20)
 pasw = Entry(janela,show="*")
-pasw.pack()
+pasw.place(x=290,y=40)
 #Saida de Dados/Exibição
 saida = Text(janela)
-saida.pack()
+saida.place(x=5,y=70)
+#Eventos/Execução de Comandos
+btn_conf = Button(janela,text="Conectar",command=lambda: Conect(saida,host.get(),user.get(),pasw.get()))
+btn_conf.place(x=430,y=35)
 janela.mainloop()
--- ssh/src/ssh/__init__.py
+++ ssh/src/ssh/__init__.py
@@ -13,7 +13,7 @@ def Conect(saida,host,user,pasw):
     p = ""
     for c in range(0, len(pasw)):
         p += "*"
-    saida.insert(0.0,f"Host:{host} User: {user} Passowd: {p}")
+    saida.insert(0.0,f"Host:{host} User: {user} Passowd: {p}\n")
     ssh = SSHClient()
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
