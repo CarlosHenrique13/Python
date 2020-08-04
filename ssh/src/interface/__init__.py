3280e72ac492009a1c9889aa53c5a653ca4b0ae2
CarlosHenrique13 <62728022+CarlosHenrique13@users.noreply.github.com>
2020-08-04 18:03:28 -0300

    interface finalizada

--- ssh/src/interface/__init__.py
+++ ssh/src/interface/__init__.py
@@ -1,14 +1,37 @@
 #Pacotes Enternos
+from tkinter.messagebox import showinfo
 from tkinter import *
-
 #Pacotes Interno
 from src.ssh import *
+
+#Vaiaveis Globais
+ssh = []
+
+#Teclas precionadas
+def env(p):
+    if str(janela.focus_get()) == ".!text":
+        if p.keysym == "Return":
+            try:
+                Enviar(ssh[0],saida.get("1.0","end-1c"))
+            except(IndexError):
+                showinfo(title='SSH', message='Não foi encontrado o servido')
+        if p.keysym == "Delete":
+            saida.delete('1.0', END)
+    else:
+        if p.keysym == "Return":
+            ssh.append(onclik())
+        if p.keysym == "Delete":
+            saida.insert(END,"\n")
+
+def onclik():
+    return Conect(conetc, host.get(), user.get(), pasw.get())
 
 #Janela
 janela = Tk()
 janela.title("SSH")
-janela.geometry("500x300+50+50")
-conetc = Label(janela,text="Conectado: 127.0.0.1")
+janela.geometry("500x400+50+50")
+janela.resizable(0,0)
+conetc = Label(janela,text="Conectado: 127.0.0.1",font=20,fg="black")
 conetc.pack()
 #Host Entrada
 Label(janela,text="Ip ou Domínio").place(x=5,y=20)
@@ -23,9 +46,10 @@ Label(janela,text="Senha").place(x=290,y=20)
 pasw = Entry(janela,show="*")
 pasw.place(x=290,y=40)
 #Saida de Dados/Exibição
-saida = Text(janela)
+saida = Text(janela,width=60,height=20)
 saida.place(x=5,y=70)
 #Eventos/Execução de Comandos
-btn_conf = Button(janela,text="Conectar",command=lambda: Conect(saida,host.get(),user.get(),pasw.get()))
+btn_conf = Button(janela,text="Conectar",command=lambda: onclik())
 btn_conf.place(x=430,y=35)
+janela.bind("<KeyPress>",env)
 janela.mainloop()
--- ssh/src/ssh/__init__.py
+++ ssh/src/ssh/__init__.py
@@ -1,21 +1,41 @@
+from tkinter.messagebox import showinfo
 from paramiko import SSHClient
 import paramiko
 
-def Conect(saida,host,user,pasw):
+
+def Conect(text,host,user,pasw):
     """
     -> Conexão SSH
     :param saida: Tela para informação
     :param host: ip ou domínio para coneção
     :param user: Usuario que for se conectar
     :param pasw: Senha corespondente ao usuario
-    :return:
+    :return: OS dddos da conexão ssh
     """
-    p = ""
-    for c in range(0, len(pasw)):
-        p += "*"
-    saida.insert(0.0,f"Host:{host} User: {user} Passowd: {p}\n")
-    ssh = SSHClient()
-    ssh.load_system_host_keys()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        #Montagem da exibição
+        p = ""
+        for c in range(0, len(pasw)):
+            p += "*"
+        text["text"] = f"Host: {host} User: {user} Passowd: {p}\n"
+        text["fg"] = "green"
 
+        #Conexão
+        ssh = SSHClient()
+        ssh.load_system_host_keys()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=host, username=user, password=pasw)
+        #Envio de Comando
+        return ssh
+    except:
+        showinfo(title='SSH', message='Não foi possivel se conectar')
+
+
+def Enviar(ssh,cmd):
+    try:
+        entrada, saida, erro = ssh.exec_command(cmd)
+        print(saida.readlines())
+        print(erro.readlines())
+    except:
+        pass
 
