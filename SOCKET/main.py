c75a0333d50b72161d58f6a72c18fa419e9368dd
CarlosHenrique13 <62728022+CarlosHenrique13@users.noreply.github.com>
2020-08-26 20:18:28 -0300

    cliente socket

--- SOCKET/main.py
+++ SOCKET/main.py
@@ -1,3 +1,7 @@
 from src.cliente import *
 from src.servido import*
 from src.interface import *
+
+
+if __name__ == '__main__':
+    
---
+++ SOCKET/src/cliente/__init__.py
@@ -0,0 +1,18 @@
+import socket
+
+def Client(self,host,porta=8585):
+        """
+        -> Cliente TCP
+        :param host: Ip de destino
+        :param porta: porta de comunicação
+        :return: None
+        """
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((host, porta))
+            dados = s.recv(1024)
+            print(dados.decode('ascii'))
+        except(ConnectionRefusedError):
+            print("[-][TCP]Nenhuma conexão pôde ser feita porque a máquina de destino as recusou ativamente")
+        except():
+            pass
